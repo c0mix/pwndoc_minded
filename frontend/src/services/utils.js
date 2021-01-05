c1b3455c5fe2fa7f2b5011277626e345b199820e
c0mix <lorenzo.comi@studenti.unimi.it>
2021-01-05 23:20:38 +0100

    first commit

---
+++ frontend/src/services/utils.js
@@ -0,0 +1,51 @@
+export default {
+  htmlEncode(html) {
+    if(typeof(html) !== "string")  return "";
+    
+    var result = html
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/&lt;img.+?src="(.*?)".+?alt="(.*?)".*?&gt;/g, '<img src="$1" alt="$2">')
+    .replace(/&lt;p&gt;/g, '<p>')
+    .replace(/&lt;\/p&gt;/g, '</p>')
+    .replace(/&lt;pre&gt;/g, '<pre>')
+    .replace(/&lt;\/pre&gt;/g, '</pre>')
+    .replace(/&lt;b&gt;/g, '<b>')
+    .replace(/&lt;\/b&gt;/g, '</b>')
+    .replace(/&lt;strong&gt;/g, '<strong>')
+    .replace(/&lt;\/strong&gt;/g, '</strong>')
+    .replace(/&lt;i&gt;/g, '<i>')
+    .replace(/&lt;\/i&gt;/g, '</i>')
+    .replace(/&lt;em&gt;/g, '<em>')
+    .replace(/&lt;\/em&gt;/g, '</em>')
+    .replace(/&lt;u&gt;/g, '<u>')
+    .replace(/&lt;\/u&gt;/g, '</u>')
+    .replace(/&lt;s&gt;/g, '<s>')
+    .replace(/&lt;\/s&gt;/g, '</s>')
+    .replace(/&lt;strike&gt;/g, '<strike>')
+    .replace(/&lt;\/strike&gt;/g, '</strike>')
+    .replace(/&lt;br&gt;/g, '<br>')
+    .replace(/&lt;code&gt;/g, '<code>')
+    .replace(/&lt;\/code&gt;/g, '</code>')
+    .replace(/&lt;ul&gt;/g, '<ul>')
+    .replace(/&lt;\/ul&gt;/g, '</ul>')
+    .replace(/&lt;ol&gt;/g, '<ol>')
+    .replace(/&lt;\/ol&gt;/g, '</ol>')
+    .replace(/&lt;li&gt;/g, '<li>')
+    .replace(/&lt;\/li&gt;/g, '</li>')
+    .replace(/&lt;h1&gt;/g, '<h1>')
+    .replace(/&lt;\/h1&gt;/g, '</h1>')
+    .replace(/&lt;h2&gt;/g, '<h2>')
+    .replace(/&lt;\/h2&gt;/g, '</h2>')
+    .replace(/&lt;h3&gt;/g, '<h3>')
+    .replace(/&lt;\/h3&gt;/g, '</h3>')
+    .replace(/&lt;h4&gt;/g, '<h4>')
+    .replace(/&lt;\/h4&gt;/g, '</h4>')
+    .replace(/&lt;h5&gt;/g, '<h5>')
+    .replace(/&lt;\/h5&gt;/g, '</h5>')
+    .replace(/&lt;h6&gt;/g, '<h6>')
+    .replace(/&lt;\/h6&gt;/g, '</h6>')
+
+    return result
+  }
+}
